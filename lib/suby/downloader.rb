92e886cd5d0f8478ec04ee33ed3052dc31eefe28
eregon <eregontp@gmail.com>
2011-07-13 14:15:51 +0200

    refactor get_*_url methods: * remove the get_ * cache their results, removing the need to use variables between them for relations * test them

--- lib/suby/downloader.rb
+++ lib/suby/downloader.rb
@@ -20,7 +20,11 @@ module Suby
       @show, @season, @episode = show, season.to_i, episode.to_i
     end
 
-    def get_show_url http
+    def http
+      @http ||= Net::HTTP.new(SITE).start
+    end
+
+    def show_url
       SHOW_URLS[show] ||= begin
         post = Net::HTTP::Post.new(SEARCH_URL)
         post.form_data = { 'q' => show }
@@ -32,60 +36,56 @@ module Suby
       end
     end
 
-    def get_episode_url http, show_url
-      show_page = (SHOW_PAGES[show] ||= Nokogiri http.get(show_url).body)
+    def episode_url
+      @episode_url ||= begin
+        show_page = (SHOW_PAGES[show] ||= Nokogiri http.get(show_url).body)
 
-      episode_url = nil
-      show_page.css('div.left_articles table tr').find { |tr|
-        tr.children.find { |td| td.name == 'td' && td.text =~ /\A#{season}x0?#{episode}\z/ }
-      }.children.find { |td|
-        td.children.find { |a|
-          a.name == 'a' && a[:href].start_with?('episode') && episode_url = a[:href]
+        url = nil
+        show_page.css('div.left_articles table tr').find { |tr|
+          tr.children.find { |td| td.name == 'td' && td.text =~ /\A#{season}x0?#{episode}\z/ }
+        }.children.find { |td|
+          td.children.find { |a|
+            a.name == 'a' && a[:href].start_with?('episode') && url = a[:href]
+          }
         }
-      }
-      raise "invalid episode url: #{episode_url}" unless episode_url =~ /^episode-(\d+)\.html$/
-      "/episode-#{$1}-#{lang}.html"
+        raise "invalid episode url: #{episode_url}" unless url =~ /^episode-(\d+)\.html$/
+        "/episode-#{$1}-#{lang}.html"
+      end
     end
 
-    def get_subtitles_url http, episode_url
-      subtitles = Nokogiri http.get(episode_url).body
+    def subtitles_url
+      @subtitles_url ||= begin
+        subtitles = Nokogiri http.get(episode_url).body
 
-      # TODO: choose 720p or most downloaded instead of first found
-      subtitle_url = subtitles.css('div.left_articles a').find { |a| a.name == 'a' && a[:href].start_with?('/subtitle') }[:href]
-      raise 'invalid subtitle url' unless subtitle_url =~ /^\/subtitle-(\d+)\.html/
-      subtitle_url
+        # TODO: choose 720p or most downloaded instead of first found
+        url = subtitles.css('div.left_articles a').find { |a| a.name == 'a' && a[:href].start_with?('/subtitle') }[:href]
+        raise 'invalid subtitle url' unless url =~ /^\/subtitle-(\d+)\.html/
+        url
+      end
     end
 
-    def get_download_url http, subtitle_url
-      URI.escape '/' + http.get(subtitle_url.sub('subtitle', 'download'))['Location']
+    def download_url
+      @download_url ||= URI.escape '/' + http.get(subtitles_url.sub('subtitle', 'download'))['Location']
     end
 
     def download
       puts "Searching subtitles for #{file}:"
       puts "Show: #{show}, Season: #{season}, Episode: #{episode}"
 
-      Net::HTTP.start(SITE) { |http|
-        show_url = get_show_url http
-        puts "show url: #{show_url}"
+      puts "show url: #{show_url}"
+      puts "episode url: #{episode_url}"
+      puts "subtitle url: #{subtitles_url}"
+      puts "download url: #{download_url}"
 
-        episode_url = get_episode_url http, show_url
-        puts "episode url: #{episode_url}"
+      # extract
+      zip = http.get(download_url).body
+      http.finish
+      open(TEMP_ARCHIVE_NAME, 'wb') { |f| f.write zip }
+      subs = Suby.extract_subs_from_archive(TEMP_ARCHIVE_NAME)
 
-        subtitle_url = get_subtitles_url http, episode_url
-        puts "subtitle url: #{subtitle_url}"
-
-        download_url = get_download_url http, subtitle_url
-        puts "download url: #{download_url}"
-
-        # extract
-        zip = http.get(download_url).body
-        open(TEMP_ARCHIVE_NAME, 'wb') { |f| f.write zip }
-        subs = Suby.extract_subs_from_archive(TEMP_ARCHIVE_NAME)
-
-        new_name = File.basename(file, File.extname(file))+File.extname(subs.first)
-        File.rename subs.first, new_name
-        puts "Renaming to #{new_name}"
-      }
+      new_name = File.basename(file, File.extname(file))+File.extname(subs.first)
+      File.rename subs.first, new_name
+      puts "Renaming to #{new_name}"
     end
   end
 end
--- spec/suby/downloader_spec.rb
+++ spec/suby/downloader_spec.rb
@@ -22,5 +22,22 @@ describe Suby::Downloader do
     }
   end
 
-  it 'finds the right show url'
+  it 'finds the right show url' do
+    Downloader.new(file).show_url.should == '/tvshow-110-3.html'
+  end
+
+  it 'finds the right episode url' do
+    Downloader.new(file).episode_url.should == '/episode-7517-en.html'
+    Downloader.new(file, :fr).episode_url.should == '/episode-7517-fr.html'
+  end
+
+  it 'finds the right subtitles url' do
+    Downloader.new(file).subtitles_url.should == '/subtitle-9339.html'
+    Downloader.new(file, :fr).subtitles_url.should == '/subtitle-31249.html'
+  end
+
+  it 'finds the right download url' do
+    Downloader.new(file).download_url.should == '/files/How%20I%20Met%20Your%20Mother_3x09_en.zip'
+    Downloader.new(file, :fr).download_url.should == '/files/How%20I%20Met%20Your%20Mother_3x09_fr.zip'
+  end
 end
