5dd1cd7131f29a7ee5ccf51fde3bca1a8f730356
eregon <eregontp@gmail.com>
2011-07-25 23:59:12 +0200

    add tests for clean_show_name and organize the FilenameParser spec

--- spec/suby/filename_parser_spec.rb
+++ spec/suby/filename_parser_spec.rb
@@ -8,25 +8,43 @@ describe Suby::Downloader do
   dot_show = show.tr(' ', '.')
   und_show = show.tr(' ', '_')
 
-  [
-    "#{show} #{season}x#{episode}",
-    "#{show} #{season}x#{"%.2d" % episode}",
-    "#{show} #{season}x#{episode} - #{title}",
-    "#{show} #{season}x#{"%.2d" % episode} - #{title}",
-    "#{dot_show}.s0309",
-    "#{dot_show}.0309",
-    "#{dot_show}.3x09",
-    "#{dot_show}.s03.e09",
-    "#{und_show}.s03_e09",
-    "#{show} - [03.09]",
-    "#{show} - S3 E 09",
-    "#{show} - Episode 9999 [S 3 - Ep 9]",
-    "#{show} - Episode 9999 [S 3 - Ep 9] - ",
-    "#{dot_show}.309",
-    "#{dot_show}.0309",
-  ].each do |filename|
-    it "parse correctly the file name #{filename}" do
-      Suby::FilenameParser.parse(filename+ext).should == [show, season, episode]
+  context "cleans correctly the show name" do
+    {
+      dot_show => show,
+      und_show => show,
+      "an.example.1.0.test" => "an example 1.0 test",
+      "an_example_1.0_test" => "an example 1.0 test",
+      "an_3.0.1.example_1.0_test" => "an 3.0.1 example 1.0 test",
+    }.each_pair { |raw_show, true_show|
+      it raw_show do
+        # dup because a literal String is frozen and no point to keep the raw show
+        Suby::FilenameParser.clean_show_name(raw_show.dup).should == true_show
+      end
+    }
+  end
+
+  context "parse correctly the file name" do
+    [
+      "#{show} #{season}x#{episode}",
+      "#{show} #{season}x#{"%.2d" % episode}",
+      "#{show} #{season}x#{episode} - #{title}",
+      "#{show} #{season}x#{"%.2d" % episode} - #{title}",
+      "#{dot_show}.s0309",
+      "#{dot_show}.0309",
+      "#{dot_show}.3x09",
+      "#{dot_show}.s03.e09",
+      "#{und_show}.s03_e09",
+      "#{show} - [03.09]",
+      "#{show} - S3 E 09",
+      "#{show} - Episode 9999 [S 3 - Ep 9]",
+      "#{show} - Episode 9999 [S 3 - Ep 9] - ",
+      "#{dot_show}.309",
+      "#{dot_show}.0309",
+    ].each do |filename|
+      it filename do
+        file = filename + ext
+        Suby::FilenameParser.parse(file).should == [show, season, episode]
+      end
     end
   end
 end
