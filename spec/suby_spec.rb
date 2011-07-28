bd285df40e7dc6e28e85a32ccfc08393937e7704
eregon <eregontp@gmail.com>
2011-07-28 14:41:06 +0200

    move the suby_spec.rb file to the right place

--- spec/suby_spec.rb
+++ spec/suby_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../spec_helper'
+require_relative 'spec_helper'
 require 'tmpdir'
 require 'digest/md5'
 
@@ -8,7 +8,7 @@ describe Suby do
 
   it 'works :D' do
     Dir.mktmpdir do |dir|
-      suby = File.expand_path('../../../bin/suby', __FILE__)
+      suby = File.expand_path('../../bin/suby', __FILE__)
       Dir.chdir(dir) do
         system suby, file
         Digest::MD5.hexdigest(File.read(srt)).should == '66c4d3b40839a957f218e043105a1352'
