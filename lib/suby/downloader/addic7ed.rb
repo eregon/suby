c6b0f9760fe72d16cf234b42bd42387813035f34
eregon <eregontp@gmail.com>
2011-07-28 04:51:18 +0200

    split the Downloader::Addic7ed#download_url method

--- lib/suby/downloader/addic7ed.rb
+++ lib/suby/downloader/addic7ed.rb
@@ -9,23 +9,35 @@ module Suby
     }
     FILTER_IGNORED = "Couldn't find any subs with the specified language. Filter ignored"
 
-    def download_url
-      subtitles_url = "/serie/#{CGI.escape show}/#{season}/#{episode}/#{LANG_IDS[lang]}"
+    def subtitles_url
+      "/serie/#{CGI.escape show}/#{season}/#{episode}/#{LANG_IDS[lang]}"
+    end
+
+    def subtitles_response
       response = http.get(subtitles_url)
       raise NotFoundError, "show/season/episode not found" unless Net::HTTPSuccess === response
+      response
+    end
 
-      body = response.body
+    def subtitles_body
+      body = subtitles_response.body
       raise NotFoundError, "no subtitle available" if body.include? FILTER_IGNORED
+      body
+    end
 
-      download_url = Nokogiri(body).css('a').find { |a|
+    def redirected_url download_url
+      location = get_redirection download_url, 'Referer' => "http://#{SITE}#{subtitles_url}" # They check Referer
+      raise NotFoundError, "download exceeded" if location == '/downloadexceeded.php'
+      URI.escape location
+    end
+
+    def download_url
+      download_url = Nokogiri(subtitles_body).css('a').find { |a|
         a[:href].start_with? '/original/' or
         a[:href].start_with? '/updated/'
       }[:href]
 
-      location = get_redirection download_url, 'Referer' => "http://#{SITE}#{subtitles_url}" # They check Referer
-      raise NotFoundError, "download exceeded" if location == '/downloadexceeded.php'
-
-      URI.escape location
+      redirected_url download_url
     end
   end
 end
