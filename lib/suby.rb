dd1d9da75c9c909ad797144f4d217473543b9a7f
eregon <eregontp@gmail.com>
2011-07-28 18:58:28 +0200

    split the download_subtitles method

--- lib/suby.rb
+++ lib/suby.rb
@@ -12,34 +12,42 @@ module Suby
   def download_subtitles(files, options = {})
     files.each { |file|
       next puts "Skipping: #{file}" if SUB_EXTENSIONS.any? { |ext|
-        File.exist? File.basename(file, File.extname(file)) + ".#{ext}" }
-
-      begin
-        success = Downloader::DOWNLOADERS.find do |downloader|
-          begin
-            downloader.new(file, options[:lang]).download
-          rescue Suby::NotFoundError => error
-            puts "#{downloader} did not find subtitles for #{file}" +
-                 " (#{error.message})"
-            false
-          rescue Suby::DownloaderError => error
-            puts "#{downloader} had a problem finding subtitles for #{file}" +
-                 " (#{error.message})"
-            false
-          else
-            puts "#{downloader} found subtitles for #{file}"
-            true
-          end
-        end
-        unless success
-          STDERR.puts "No downloader could find subtitles for #{file}"
-        end
-      rescue
-        puts "  The download of the subtitles failed for #{file}:"
-        puts "  #{$!.class}: #{$!.message}"
-        puts $!.backtrace.map { |line| line.prepend ' '*4 }
-      end
+        File.exist? File.basename(file, File.extname(file)) + ".#{ext}"
+      }
+      download_subtitles_for_file(file, options)
     }
+  end
+
+  def download_subtitles_for_file(file, options)
+    begin
+      success = Downloader::DOWNLOADERS.find { |downloader_class|
+        try_downloader(downloader_class.new(file, options[:lang]))
+      }
+      unless success
+        STDERR.puts "No downloader could find subtitles for #{file}"
+      end
+    rescue
+      puts "  The download of the subtitles failed for #{file}:"
+      puts "  #{$!.class}: #{$!.message}"
+      puts $!.backtrace.map { |line| line.prepend ' '*4 }
+    end
+  end
+
+  def try_downloader(downloader)
+    begin
+      downloader.download
+    rescue Suby::NotFoundError => error
+      puts "#{downloader.class} did not find subtitles for " +
+           "#{downloader.file} (#{error.message})"
+      false
+    rescue Suby::DownloaderError => error
+      puts "#{downloader.class} had a problem finding subtitles for " +
+           "#{downloader.file} (#{error.message})"
+      false
+    else
+      puts "#{downloader.class} found subtitles for #{downloader.file}"
+      true
+    end
   end
 
   def extract_sub_from_archive(archive, format, basename)
