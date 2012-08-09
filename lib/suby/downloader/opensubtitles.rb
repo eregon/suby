0ca3d6b6bead0dfcb23d62541f860660dc2f11d5
zewelor <zewelor@gmail.com>
2012-08-09 16:14:40 +0200

    language search option handled

--- lib/suby/downloader/opensubtitles.rb
+++ lib/suby/downloader/opensubtitles.rb
@@ -10,7 +10,17 @@ module Suby
     LANGUAGE = 'eng'
     USER_AGENT = 'SubDownloader 2.0.10'
 
-    def subtitles_url
+    # OpenSubtitles needs ISO 639-2 language codes for subtitles search
+    # http://en.wikipedia.org/wiki/List_of_ISO_639-2_codes
+    LANG_MAPPING = {
+      en: 'eng', es: 'spa', it: 'ita', fr: 'fra', pt: 'por', de: 'deu', ca: 'cat', eu: 'eus', cs: 'ces', gl: 'glg',
+      tr: 'tur', nl: 'nld', sv: 'swe', ru: 'rus', hu: 'hun', pl: 'pol', sl: 'slv', he: 'heb', zh: 'zho', sk: 'slk',
+      ro: 'ron', el: 'ell', fi: 'fin', da: 'dan', hr: 'hrv', ja: 'jpn', bg: 'bul', sr: 'srp', id: 'ind', ar: 'ara',
+      ms: 'msa', ko: 'kor', fa: 'fas', bs: 'bos', vi: 'vie', th: 'tha', bn: 'ben', no: 'nor'
+    }
+    LANG_MAPPING.default = 'all'
+
+    def subtitles_url(query = subtitles_search_query)
       subs = @client.call('SearchSubtitles', token, query)['data']
       raise NotFoundError, "no subtitle available" unless subs
       subs.first['ZipDownloadLink']
@@ -23,13 +33,19 @@ module Suby
     def login
       response = @client.call('LogIn', USERNAME, PASSWORD, LANGUAGE, USER_AGENT)
       unless response['status'] == '200 OK'
-        raise DownloaderError "Failed to login with #{USERNAME} : #{PASSWORD}. Server return code: #{response['status']}"
+        raise DownloaderError "Failed to login with #{USERNAME} : #{PASSWORD}. " +
+                              "Server return code: #{response['status']}"
       end
       response['token']
     end
 
-    def query
-      [{'moviehash' => MovieHasher.compute_hash(@file.path), 'moviebytesize' => @file.size, 'sublanguageid' => 'eng'}]
+    def subtitles_search_query
+      [{'moviehash' => MovieHasher.compute_hash(@file.path), 'moviebytesize' => @file.size,
+        'sublanguageid' => language(lang)}]
+    end
+
+    def language(lang)
+      LANG_MAPPING[lang.to_sym]
     end
 
     def download_url
